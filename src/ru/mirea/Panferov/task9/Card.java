e951c30fe129b5b89f141530883ba49538116ca3
Egor Panferov <egor.panfyorof@yandex.ru>
2022-10-12 02:15:38 +0300

    Доделал покер

--- src/ru/mirea/Panferov/task9/Card.java
+++ src/ru/mirea/Panferov/task9/Card.java
@@ -8,5 +8,23 @@ public class Card {
         this.master = master;
         this.dignity = dignity;
     }
+    public Card(){}
 
+    public void setMaster(Masters master) { this.master = master; }
+    public void setDignity(Dignities dignity) { this.dignity = dignity; }
+
+    @Override
+    public String toString() {
+        return master + " " + dignity;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) return true;
+        if (obj == null) return false;
+
+        Card card = (Card) obj;
+        return this.dignity == card.dignity && this.master == card.master;
+    }
 }
--- src/ru/mirea/Panferov/task9/Poker.java
+++ src/ru/mirea/Panferov/task9/Poker.java
@@ -8,15 +8,53 @@ public class Poker  {
         Scanner scanner = new Scanner(System.in);
         int n;
         while(true){
-            System.out.println("Введите количество игроков: ");
+            System.out.print("Введите количество игроков: ");
             n = scanner.nextInt();
             if (n <= 7)
                 break;
+        }
+
+        for (int i = 0; i < n*5; i++){
+            cards[i] = createCard(cards);
+        }
+
+        int index = 0;
+        for (int i = 1; i <= n; i++){
+            System.out.print("==========================\n");
+            System.out.printf("Карты %d игрока: \n", i);
+            for(int j = 0; j < 5; j++){
+                System.out.println(cards[index]);
+                index++;
+            }
         }
     }
 
     static public Card createCard(Card[] cards){
         int masterOrd = (int)(Math.random()*4);
-        int DignityOrd = (int)(Math.random()*9);
+        int dignityOrd = (int)(Math.random()*9);
+        Card cardFunc = new Card();
+
+        switch (masterOrd) {
+            case 0 -> cardFunc.setMaster(Masters.Club);
+            case 1 -> cardFunc.setMaster(Masters.Diamond);
+            case 2 -> cardFunc.setMaster(Masters.Heart);
+            case 3 -> cardFunc.setMaster(Masters.Spades);
+        }
+        switch (dignityOrd) {
+            case 0 -> cardFunc.setDignity(Dignities.Six);
+            case 1 -> cardFunc.setDignity(Dignities.Seven);
+            case 2 -> cardFunc.setDignity(Dignities.Eight);
+            case 3 -> cardFunc.setDignity(Dignities.Nine);
+            case 4 -> cardFunc.setDignity(Dignities.Ten);
+            case 5 -> cardFunc.setDignity(Dignities.Jack);
+            case 6 -> cardFunc.setDignity(Dignities.Queen);
+            case 7 -> cardFunc.setDignity(Dignities.King);
+            case 8 -> cardFunc.setDignity(Dignities.Ace);
+        }
+
+        for (Card card : cards) if (cardFunc.equals(card)){
+            cardFunc = createCard(cards);
+        }
+        return cardFunc;
     }
 }
